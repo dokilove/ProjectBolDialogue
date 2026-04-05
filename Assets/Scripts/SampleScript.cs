94fdadef6a9a78871b102ef3e19a1d86e3a47931
dokilove <dokilove2@gmail.com>
2026-04-06 01:02:49 +0900

    샘플로 만들어봄

---
+++ Assets/Scripts/SampleScript.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using PixelCrushers.DialogueSystem;
+
+public class SampleScript : MonoBehaviour
+{
+    void Start()
+    {
+        DialogueManager.StartConversation("Event2-3");
+    }
+}
